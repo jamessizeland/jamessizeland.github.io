b0a39a853e883c296cea1f78b6ddaf4b81e14c49
James Sizeland <jamessizeland@gmail.com>
2025-05-09 22:41:36 +0100

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
@@ -7,4 +7,5 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsConfigPaths(), svgr()],
-})
+  base: "/",
+});
